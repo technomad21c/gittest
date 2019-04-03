88a2872a6ca0846ad4f11b15d1d29f371bf9f7ca
Brandon Jeong <handeum@Charlotte-Katakuris-iMac.local>
2019-04-02 22:15:01 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from add import add
+from subtract import subtract
+from multiply import multiply
+from divide import divide
+
+a = 10
+b = 7
+
+print("Result from add method: ", add(a,b))
+print("Result from subtract method: ", subtract(a,b))
+print("Result from multiply method: ", multiply(a,b))
+print("Result from divide method: ", divide(a,b))
